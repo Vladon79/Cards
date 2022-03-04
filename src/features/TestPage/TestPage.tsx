4653a7ef5065e38ed6ad805b20d2ef36b2ca8b51
Vladon79 <vla3ik@gmail.com>
2022-03-04 20:33:20 +0300

    preloader_commit

--- src/features/TestPage/TestPage.tsx
+++ src/features/TestPage/TestPage.tsx
@@ -4,11 +4,13 @@ import SuperInputText from "../../ui/common/c1-SuperInputText/SuperInputText";
 import s from './TestPage.module.scss'
 import blockStyle from '../../styles/container.module.scss'
 import Preloader from "../../ui/common/Preloader/Preloader";
+import React from "react";
 
 const TestPage = () => {
     return (
         <div className={`${s.testContainer} ${blockStyle.container}`}>
             <h2>Test page</h2>
+            <Preloader/>
             <SuperCheckbox children={'hello'}/>
             <div className={s.buttonInputBlock}>
                 <SuperInputText></SuperInputText>
