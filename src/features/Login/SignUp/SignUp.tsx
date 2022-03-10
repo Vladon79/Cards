27680d1827c3e87506e089ed7c3fdea7712833af
nimrab <nimrab.0@gmail.com>
2022-03-10 19:54:07 +0300

    SignUp added loader

--- src/features/Login/SignUp/SignUp.tsx
+++ src/features/Login/SignUp/SignUp.tsx
@@ -3,12 +3,11 @@ import s from './SignUp.module.scss'
 import {register} from "../../../bll/reducers/sign-up-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import SuperButton from "../../../ui/common/c2-SuperButton/SuperButton";
-import SuperInputText from "../../../ui/common/c1-SuperInputText/SuperInputText";
 import {Navigate, useNavigate} from "react-router-dom";
-import eye from '../../../assets/icons/eyeicon.png'
 import {AppRootStateType} from "../../../bll/store";
 import {AuthEmailField} from "../../../ui/common/AuthFields/AuthEmailField/AuthEmailField";
 import {AuthPassField} from "../../../ui/common/AuthFields/AuthPassField/AuthPassField";
+import Preloader from "../../../ui/common/Preloader/Preloader";
 
 type InputFieldType = 'password' | 'text'
 
@@ -17,6 +16,7 @@ export const SignUp = () => {
     const navigate = useNavigate()
 
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+    const isFetching = useSelector<AppRootStateType, boolean>(state => state.app.isFetching)
     const dispatch = useDispatch()
 
     const [email, setEmail] = useState<string>('')
@@ -48,55 +48,59 @@ export const SignUp = () => {
 
     return (
         <section className={s.main_box}>
-            <section className={s.sign_up_box}>
-                <div className={s.sign_up_box_header}>
-                    <div className={s.logo_text}>It-incubator</div>
-                    <div className={s.sign_up_text}>Sign Up</div>
-                </div>
-                <form>
-                    <div className={s.input_box_form}>
-
-                        <AuthEmailField
-                            email={email}
-                            setEmail={setEmail}
-                        />
-
-                        <AuthPassField
-                            type={passwordInputMode}
-                            password={password}
-                            isShowPassword={isShowPassword}
-                            setPassword={setPassword}
-                            showPassword={showPassword}
-                            text={'Password'}
-                        />
-
-                        <AuthPassField
-                            type={repeatPasswordInputMode}
-                            password={repeatPassword}
-                            isShowPassword={isShowRepeatPassword}
-                            setPassword={setRepeatPassword}
-                            showPassword={showRepeatPassword}
-                            text={'Confirm password'}
-                        />
-
+            {isFetching
+                ? <Preloader/>
+                :
+                <section className={s.sign_up_box}>
+                    <div className={s.sign_up_box_header}>
+                        <div className={s.logo_text}>It-incubator</div>
+                        <div className={s.sign_up_text}>Sign Up</div>
                     </div>
-                    <div className={s.input_box_buttons}>
-                        <SuperButton
-                            className={s.btn_cancel}
-                            onClick={() => navigate('/')}
-                        >
-                            Cancel
-                        </SuperButton>
+                    <form>
+                        <div className={s.input_box_form}>
 
-                        <SuperButton
-                            className={s.btn_register}
-                            onClick={(e) => registerBtnClickHandler(e)}
-                        >
-                            Register
-                        </SuperButton>
-                    </div>
-                </form>
-            </section>
+                            <AuthEmailField
+                                email={email}
+                                setEmail={setEmail}
+                            />
+
+                            <AuthPassField
+                                type={passwordInputMode}
+                                password={password}
+                                isShowPassword={isShowPassword}
+                                setPassword={setPassword}
+                                showPassword={showPassword}
+                                text={'Password'}
+                            />
+
+                            <AuthPassField
+                                type={repeatPasswordInputMode}
+                                password={repeatPassword}
+                                isShowPassword={isShowRepeatPassword}
+                                setPassword={setRepeatPassword}
+                                showPassword={showRepeatPassword}
+                                text={'Confirm password'}
+                            />
+
+                        </div>
+                        <div className={s.input_box_buttons}>
+                            <SuperButton
+                                className={s.btn_cancel}
+                                onClick={() => navigate('/')}
+                            >
+                                Cancel
+                            </SuperButton>
+
+                            <SuperButton
+                                className={s.btn_register}
+                                onClick={(e) => registerBtnClickHandler(e)}
+                            >
+                                Register
+                            </SuperButton>
+                        </div>
+                    </form>
+                </section>
+            }
         </section>
     )
 }
