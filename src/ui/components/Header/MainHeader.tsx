b2ffedb6d27d63da56471352889ac0f8ba4021df
nimrab <nimrab.0@gmail.com>
2022-03-10 17:16:51 +0300

    disable header while fetching

--- src/ui/components/Header/MainHeader.tsx
+++ src/ui/components/Header/MainHeader.tsx
@@ -1,18 +1,35 @@
 import {NavLink} from "react-router-dom"
 import s from './Header.module.scss'
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../../bll/store";
 
 
 const Header = () => {
-    return (
-        <header>
-            <NavLink children={'Sign In'} to={'/signin'} className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
-            <NavLink children={'Sign Up'} to={'/signup'} className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
-            <NavLink children={'Profile'} to={'/profile'}
-                     className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
-            <NavLink children={'Error'} to={'/error'} className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
-            <NavLink children={'Test'} to={'/test'} className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
-        </header>
-    )
+
+    const isFetching = useSelector<AppRootStateType, boolean>(state => state.app.isFetching)
+
+
+    if (isFetching) {
+        return (
+            <header></header>
+        )
+    } else {
+
+        return (
+            <header>
+                <NavLink children={'Sign In'} to={'/signin'}
+                         className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
+                <NavLink children={'Sign Up'} to={'/signup'}
+                         className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
+                <NavLink children={'Profile'} to={'/profile'}
+                         className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
+                <NavLink children={'Error'} to={'/error'}
+                         className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
+                <NavLink children={'Test'} to={'/test'} className={({isActive}) => (isActive ? s.linkActive : s.link)}/>
+
+            </header>
+        )
+    }
 }
 
 export default Header
